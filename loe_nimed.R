da84717303d7ea4d5a03a2d2575924dc31bbe748
taivop <taivo.pungas@gmail.com>
2015-10-31 20:04:48 +0100

    committing for backup

--- loe_nimed.R
+++ loe_nimed.R
@@ -1,29 +1,37 @@
 library(dplyr)
 
-naised <- list()
-lines <- readLines("nimed/naised.txt")
-for(line in lines) {
-  uued_nimed <- strsplit(line, "\t")
-  naised <- unlist(c(naised, uued_nimed))
-}
-
-mehed <- list()
-lines <- readLines("nimed/mehed.txt")
-for(line in lines) {
-  uued_nimed <- strsplit(line, "\t")
-  mehed <- unlist(c(mehed, uued_nimed))
-}
-
-dfn <- as.data.frame(naised) %>% rename(Nimi=naised)
-dfm <- as.data.frame(mehed) %>% rename(Nimi=mehed)
-dfm <- dfm %>% mutate(Mees=TRUE)
-dfn <- dfn %>% mutate(Naine=TRUE)
-
-nimetabel <- rbind(dfm %>% select(Nimi), dfn %>% select(Nimi)) %>%
-  group_by(Nimi) %>%
-  filter(row_number(Nimi) == 1) %>%
-  left_join(dfm, by="Nimi") %>%
-  left_join(dfn, by="Nimi") %>%
-  mutate(Mees=ifelse(is.na(Mees), FALSE, TRUE)) %>%
-  mutate(Naine=ifelse(is.na(Naine), FALSE, TRUE))
+loo_nimetabel = function() {
   
+  # Loeme naisenimed listi
+  naised <- list()
+  lines <- readLines("nimed/naised.txt")
+  for(line in lines) {
+    uued_nimed <- strsplit(line, "\t")
+    naised <- unlist(c(naised, uued_nimed))
+  }
+  
+  # Loeme mehenimed listi
+  mehed <- list()
+  lines <- readLines("nimed/mehed.txt")
+  for(line in lines) {
+    uued_nimed <- strsplit(line, "\t")
+    mehed <- unlist(c(mehed, uued_nimed))
+  }
+  
+  # Ühendame kaks listi
+  dfn <- as.data.frame(naised) %>% rename(Nimi=naised)
+  dfm <- as.data.frame(mehed) %>% rename(Nimi=mehed)
+  dfm <- dfm %>% mutate(Mees=TRUE)
+  dfn <- dfn %>% mutate(Naine=TRUE)
+  
+  # Paneme nimetabeli kokku
+  nimetabel <- rbind(dfm %>% select(Nimi), dfn %>% select(Nimi)) %>%
+    group_by(Nimi) %>%
+    filter(row_number(Nimi) == 1) %>%  # Eemaldame duplikaadid
+    left_join(dfm, by="Nimi") %>%
+    left_join(dfn, by="Nimi") %>%
+    mutate(Mees=ifelse(is.na(Mees), FALSE, TRUE)) %>%
+    mutate(Naine=ifelse(is.na(Naine), FALSE, TRUE))
+  
+  return(nimetabel)
+}
--- palgalohe.R
+++ palgalohe.R
@@ -5,6 +5,7 @@ library(reshape2)
 library(ggplot2)
 
 source("loe_nimed.R")
+nimetabel = loo_nimetabel()
 
 # Loe failid sisse
 palgad_kov <- read_excel("data/palgad_kov.xlsx",
@@ -43,11 +44,44 @@ vastetud <- palgad %>%
   summarise(count=n()) %>%
   arrange(desc(count))
 
+
+# ---- Graafikud ja analüüs ----
 ggplot(palgad) +
   geom_bar(aes(x=Täiskoormusega, fill=Sugu)) +
   facet_wrap(~Tüüp, nrow=2, ncol=1, scales="free_y")
 
-g <- palgad %>%
-  group_by(Asutus, Struktuuriüksus, Ametikoht) %>%
-  summarise(count=n()) %>%
-  filter(count > 1)
+
+# Asutuse kaupa keskmiste palkade vahe
+lohe <- palgad %>%
+  group_by(Tüüp, Asutus, Ametikoht) %>%
+  summarise(KeskminePalkN=mean(ifelse(Naine, Täiskoormusega, 0)), # TODO kas mu arvutused üldse meigivad senssi?
+            KeskminePalkM=mean(ifelse(Mees , Täiskoormusega, 0)),
+            countN=sum(ifelse(Naine, 1, 0)),
+            countM=sum(ifelse(Mees, 1, 0))) %>%
+  summarise(SummaPalkN=sum(KeskminePalkN*countN),
+            SummaPalkM=sum(KeskminePalkM*countM),
+            totalcountN=sum(countN),
+            totalcountM=sum(countM)) %>%
+  mutate(KeskmineN=SummaPalkN/totalcountN,
+         KeskmineM=SummaPalkM/totalcountM) %>%
+  ungroup() %>%
+  filter(totalcountN >= 3) %>%
+  filter(totalcountM >= 3)
+
+ggplot(lohe) +
+  geom_point(aes(x=KeskmineM, y=KeskmineN, color=Tüüp,
+                 size=2000*sqrt(totalcountN+totalcountM))) +
+  geom_abline(alpha=1, colour="#555555") +
+  geom_abline(alpha=0.5, colour="#aaaaaa", slope=0.6) +
+  geom_abline(alpha=0.5, colour="#aaaaaa", slope=0.8) +
+  geom_abline(alpha=0.5, colour="#aaaaaa", slope=1.2) +
+  geom_abline(alpha=0.5, colour="#aaaaaa", slope=1.4) +
+  xlim(0, 2500) +
+  ylim(0, 2500) +
+  theme_bw() +
+  theme(panel.grid.minor=element_blank())
+
+
+
+
+
